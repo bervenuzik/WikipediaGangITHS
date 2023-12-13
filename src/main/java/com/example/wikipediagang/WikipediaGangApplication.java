b6a37c3e5c2129b25e628a70f2343f505da889ce
Vladyslav Berveno <vladundancer@gmail.com>
2023-12-13 14:26:49 +0100

    -------Project initialization -------

---
+++ src/main/java/com/example/wikipediagang/WikipediaGangApplication.java
@@ -0,0 +1,13 @@
+package com.example.wikipediagang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WikipediaGangApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WikipediaGangApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/wikipediagang/WikipediaGangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wikipediagang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WikipediaGangApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
