af0b286e69ad76725acf85e3b9d9adbf2f9ba44e
Sofie Van Dingenen <sofie.van.dingenen@iths.se>
2023-12-18 10:56:18 +0100

    added find method in loginRepository

--- src/main/java/com/example/wikipediagang/Model/LoginInformationRepo.java
+++ src/main/java/com/example/wikipediagang/Model/LoginInformationRepo.java
@@ -4,8 +4,13 @@ import com.example.wikipediagang.Model.LoginInformation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.OptionalInt;
+
 @Repository
 public interface LoginInformationRepo  extends JpaRepository<LoginInformation, Integer> {
 
+    Optional<Person> findByUserNameAAndPassword(String userName , String password);
 
 }
