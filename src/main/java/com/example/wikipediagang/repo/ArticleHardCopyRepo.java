832122489fafdfb70ac23b8029eaab3bcf794884
Depinder Kaur <depinder.kaur@iths.se>
2023-12-19 16:28:43 +0100

    Added a query in ArticleHardCopyRepo to find the number of hard-copies of an article using article's id.

--- src/main/java/com/example/wikipediagang/repo/ArticleHardCopyRepo.java
+++ src/main/java/com/example/wikipediagang/repo/ArticleHardCopyRepo.java
@@ -2,8 +2,12 @@ package com.example.wikipediagang.repo;
 
 import com.example.wikipediagang.model.ArticleHardCopy;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ArticleHardCopyRepo extends JpaRepository<ArticleHardCopy, Integer> {
+    @Query(nativeQuery = true,
+            value = "select count(article_id) from article_hard_copy a group by article_id having article_id=:articleId")
+    int findNumberOfHardCopiesByArticleId(Integer articleId);
 }
