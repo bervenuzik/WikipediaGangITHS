30f0f7bae6f63e776aa3a693fc9b63e58378f89e
LLLLLLisaa <xianochchu@gmail.com>
2024-01-07 20:33:14 +0100

    1.creat ErrorLoggingAspect to get all exception if happens to save in errorlog database 2.uppdate ErrorService method saveErrorLogs to get person-id if have can save to errorlog database 3.logback.xml is the configuration file for Logback logging frame work. 4.uppdate properties for log level error

--- src/main/java/com/example/wikipediagang/service/ErrorLogService.java
+++ src/main/java/com/example/wikipediagang/service/ErrorLogService.java
@@ -4,28 +4,50 @@ import com.example.wikipediagang.ScannerHelper;
 import com.example.wikipediagang.model.ErrorLog;
 import com.example.wikipediagang.model.Person;
 import com.example.wikipediagang.repo.ErrorLogRepo;
+import com.example.wikipediagang.repo.PersonRepository;
+import jakarta.persistence.EntityManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
+import java.util.List;
 import java.util.Optional;
 
 @Component
 public class ErrorLogService {
-    @Autowired
-    private ErrorLogRepo errorLogRepo;
+    private static ErrorLogRepo errorLogRepo ;
 
-    public void saveErroLog(){
-        ErrorLog errorLog = new ErrorLog();
-        try{
-            errorLog.setPerson(new Person());
-            errorLog.setDate(new Date());
-            errorLog.setText("Technical fail");
-            errorLog.setStatus("unchecked");
-            errorLogRepo.save(errorLog);
-        }catch(Exception e){
-            errorLog.setText("Error"+e.getStackTrace());
-        }
+    //private static final Logger logger = LoggerFactory.getLogger();
+
+
+    @Autowired
+    public ErrorLogService(ErrorLogRepo errorLogRepo) {
+        this.errorLogRepo = errorLogRepo;
+    }
+    @Autowired
+    static PersonRepository personRepo;
+
+    public static void handleException(Throwable e, String additionalInfo){
+        String errorMessage = "Exception occurred:" + e.getMessage()+". Additional Info: "+additionalInfo;
+        saveErroLog(errorMessage,null);
+    }
+
+    public static void saveErroLog(String errorMessage,Person person){
+
+            ErrorLog errorLog = new ErrorLog();
+            try{
+                errorLog.setPerson(person);
+                errorLog.setDate(new Date());
+                errorLog.setText(errorMessage);
+                errorLog.setStatus("unchecked");
+                errorLogRepo.save(errorLog);
+            }catch(Exception e){
+                errorLog.setText("Error"+e.getStackTrace());
+            }
+
+
     }
     public void uppdateErroLog(){
         System.out.println("input text");
