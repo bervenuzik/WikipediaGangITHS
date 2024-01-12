2993be8a472acce75be2faf39f1123e6f49045b9
Depinder Kaur <depinder.kaur@iths.se>
2024-01-12 15:13:16 +0100

    Modified admin menu so that he cannot write an article, but can see his reservations and return a reserved hard-copy of a borrowed article.

--- src/main/java/com/example/wikipediagang/menu/AdminMenu.java
+++ src/main/java/com/example/wikipediagang/menu/AdminMenu.java
@@ -6,7 +6,9 @@ public enum AdminMenu implements MenuOption{
     REMOVE_USER("Remove a user"),
     EDIT_USER("Update a user"),
     SEARCH("Search/Read/Reserve an article"),
-    WRITE("Write an article"),
+    SHOW_RESERVED_HARD_COPIES("Show my reserved hard-copies"),
+    SHOW_RESERVATIONS_IN_QUEUE("Show reservations in queue"),
+    RETURN_RESERVED("Return a reserved hard-copy"),
     DELETE("Delete an article"),
     EDIT_CATEGORY_LIST("Edit name of an existing category"),
     REVIEW("Review an article");
